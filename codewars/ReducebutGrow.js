dea2afca3620a51f8b22da83b4b614a2bfeb1062
ParLelya <parlelya@vk.com>
2022-09-02 17:57:33 +0300

    Init commit

---
+++ codewars/ReducebutGrow.js
@@ -0,0 +1,11 @@
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+const array = [1, 2, 3, 4]
+function multiply(arr) {
+	return arr.reduce(
+		(previousValue, currentValue) => previousValue * currentValue,
+		1)
+}
+console.log(multiply(array))
